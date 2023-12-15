db4cb73f36b3321b97f87a5cec08d92e4dc38902
vaibhavviking <chandravaibhav65@gmail.com>
2023-12-15 11:40:34 +0530

    added some styles

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <>
+        <Navbar expand="lg" className="tw-bg-orange-500">
+            <Container>
+                <Navbar.Brand href="#home" style={{margin:"auto"}} className=' tw-font-bold'>AI Trip Assistant</Navbar.Brand>
+            </Container>
+        </Navbar>
+        <App />
+    </>
 );
 
